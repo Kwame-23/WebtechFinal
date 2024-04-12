b800c701b0a52a6dfd452c4271c68642e0eff3a7
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-04-12 13:41:56 +0000

    feedback

--- actions/feedback_action.php
+++ actions/feedback_action.php
@@ -1,33 +1,25 @@
 <?php
 session_start();
-
 include("../settings/connection.php");
-
-
 date_default_timezone_set('UTC');
-$date_today=date('Y-m-d');
+$date_today = date('Y-m-d');
 
+if (isset($_POST['submit'])) {
+    $userID = $_SESSION['user_id'];
+    $feedback_content = mysqli_real_escape_string($conn, $_POST['feedback_content']);
 
+    $sql = "INSERT INTO Feedback (UserID, ReviewText, Timestamp) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('iss', $userID, $feedback_content, $date_today);
 
-if(isset($_POST['submit'])){
-    $userID= $_SESSION['user_id'];
-    $feedback_content=$_POST['feedback_content'];
-    
-
-
-    $sql = "INSERT into Feedback(UserID, ReviewText, Timestamp) values(?, ?, ?)";
-    $stmt = $conn->prepare($sql); 
-    $stmt->bind_param('iss', $userID, $feedback_content,  $date_today);  
-
-    if($stmt->execute()){
-        header("Location: ../templates/dashboard.php");
-        echo 'Sent';
-    }
-    else{
-        echo "Error" . $conn->error;
+    if ($stmt->execute()) {
+        header("Location: ../views/dashboard.php");
         exit;
+    } else {
+        echo "Error: " . $stmt->error;
     }
-    
 
-
+    $stmt->close();
+    $conn->close();
 }
+?>
