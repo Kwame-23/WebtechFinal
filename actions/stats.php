5959c958fd9e07d71190e89badab0a9242c850fd
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-04-12 09:40:31 +0000

    stats

--- actions/stats.php
+++ actions/stats.php
@@ -4,68 +4,71 @@ include ("../settings/connection.php");
 
 
 function displayDogStatusPieChart() {
-global $conn;
- 
+    global $conn;
 
-$sql = "SELECT AvailabilityStatuses.StatusID, AvailabilityStatuses.StatusName, COUNT(Dogs.DogID) AS DogCount 
-FROM Dogs 
-INNER JOIN AvailabilityStatuses ON Dogs.StatusID = AvailabilityStatuses.StatusID
-GROUP BY AvailabilityStatuses.StatusID, AvailabilityStatuses.StatusName";
-$result = $conn->query($sql);
+    // Fetch the count of dogs for each status
+    $sql = "SELECT AvailabilityStatuses.StatusID, AvailabilityStatuses.StatusName, COUNT(Dogs.DogID) AS DogCount 
+            FROM Dogs 
+            INNER JOIN AvailabilityStatuses ON Dogs.StatusID = AvailabilityStatuses.StatusID
+            GROUP BY AvailabilityStatuses.StatusID, AvailabilityStatuses.StatusName";
+    $result = $conn->query($sql);
 
-// Prepare the data for the pie chart
-$data = array();
-$total_dogs = 0;
-while ($row = $result->fetch_assoc()) {
-$data[] = array($row['StatusName'], $row['DogCount']);
-$total_dogs += $row['DogCount'];
-}
+    // Prepare the data for the pie chart
+    $data = array();
+    $total_dogs = 0;
+    while ($row = $result->fetch_assoc()) {
+        $data[$row['StatusName']] = $row['DogCount'];
+        $total_dogs += $row['DogCount'];
+    }
 
-// Close the database connection
-$conn->close();
+    // Close the database connection
+    $conn->close();
 
-// Generate the pie chart using Google Charts
-?>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+    // Generate the pie chart using HTML/CSS
+    ?>
+    <style>
+        .pie-chart {
+            width: 300px;
+            height: 300px;
+            position: relative;
+            font-size: 16px;
+        }
+        .pie-chart .slice {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            clip: rect(0, 300px, 300px, 150px);
+        }
+        .pie-chart .slice.blue {
+            clip: rect(0, 150px, 300px, 0);
+            background-color: #0000FF;
+        }
+        .pie-chart .slice.green {
+            clip: rect(0, 225px, 300px, 150px);
+            background-color: #00FF00;
+        }
+        .pie-chart .slice.yellow {
+            clip: rect(0, 300px, 300px, 225px);
+            background-color: #FFFF00;
+        }
+        .pie-chart .label {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
 
-function drawChart() {
-var data = google.visualization.arrayToDataTable(<?php echo json_encode($data); ?>);
-
-var options = {
-    title: 'Dogs by Status',
-    pieHole: 0.4,
-    colors: ['#0000FF', '#00FF00', '#FFFF00']
-};
-
-var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-// Calculate the start and end angles for each slice
-var angles = [];
-var startAngle = 0;
-for (var i = 0; i < data.length; i++) {
-    var sliceAngle = (data[i][1] / <?php echo $total_dogs; ?>) * 360;
-    angles.push({
-        startAngle: startAngle,
-        endAngle: startAngle + sliceAngle
-    });
-    startAngle += sliceAngle;
-}
-
-// Set the start and end angles for each slice
-options.sliceVisibilityThreshold = 0;
-options.pieSliceText = 'none';
-options.pieStartAngle = angles[0].startAngle;
-options.pieSliceTextStyle = {
-    color: 'white',
-    fontSize: 16
-};
-
-chart.draw(data, options);
-}
-</script>
-<div id="piechart" style="width: 900px; height: 500px;"></div>
-<?php
+    <div class="pie-chart">
+        <?php
+        $blue_angle = $data['1'] / $total_dogs * 360;
+        $green_angle = $data['2'] / $total_dogs * 360;
+        $yellow_angle = $data['3'] / $total_dogs * 360;
+        ?>
+        <div class="slice blue" style="transform: rotate(<?php echo $blue_angle; ?>deg);"></div>
+        <div class="slice green" style="transform: rotate(<?php echo $blue_angle + $green_angle; ?>deg);"></div>
+        <div class="slice yellow" style="transform: rotate(<?php echo $blue_angle + $green_angle + $yellow_angle; ?>deg);"></div>
+        <div class="label"><?php echo $total_dogs; ?> Dogs</div>
+    </div>
+    <?php
 }
